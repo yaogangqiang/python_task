2b8519376e0818dc7b2b3c33bd5521e023173507
Andy Yao <yaogangqiang@admaster.com.cn>
2013-01-30 11:30:21 +0800

    finished 2

--- 2.py
+++ 2.py
@@ -1,1 +1,24 @@
-numbers_set = set(range(1, 10))
+def is_pandigital(*args):
+    num = sorted(''.join(str(arg) for arg in args))
+    length = 9
+    if len(num) != length:
+        return False
+
+    for i in range(len(num)):
+        if str(i + 1) != str(num[i]):
+            return False
+    return True
+
+
+def main():
+    pandigitals = set()
+    total = 0
+    for multiplicand in range(100, 5000):
+        for multiplier in range(1, 100):
+            product = multiplicand * multiplier
+            if is_pandigital(multiplicand, multiplier, product):
+                pandigitals.add(product)
+    print sum(pandigitals)
+
+if __name__ == "__main__":
+    main()
